7dbf36249623c9f878002be7ced6609a2819fac9
Ant Barnes <anthony.barnes@thefloow.com>
2018-08-11 16:43:57 +0100

    Change StartMiddle strategy
    Instead of going up in down in alternating groups of 1000 alternate
    between successive higher and lower numbers starting at 5000.
    
    Also remove the results files from the repo to save having them change
    with every commit.

--- start_middle_strategy.rb
+++ start_middle_strategy.rb
@@ -9,22 +9,23 @@ class StartMiddleStrategy
 
   def run(padlock)
     @padlock = padlock
-    up_groups = []
-    [5000, 6000, 7000, 8000, 9000].each do |n|
-      up_groups << (n..n+999).to_a
-    end
 
-    down_groups = []
-    [4000, 3000, 2000, 1000, 0].each do |n|
-      down_groups << (n..n+999).to_a.reverse
-    end
-
-    codes = up_groups.zip(down_groups).flatten
-    check_range(codes)
+    check_order = create_check_order(5000)
+    check_range(check_order)
     result_hash
   end
 
   private
+
+  def create_check_order(start_point)
+    up = (start_point..9999).to_a
+    down = (0..(start_point-1)).to_a.reverse
+
+    longest = up.length > down.length ? up : down
+    shortest = longest == up ? down : up
+
+    longest.zip(shortest).flatten.compact
+  end
 
   def check_range(range)
     range.each do |n|
